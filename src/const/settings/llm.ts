b43df258b98a8d23a4db5c1a7280cff99ccefc13
briankang <285524570@qq.com>
2025-02-12 20:08:54 +0800

    Update llm.ts

--- src/const/settings/llm.ts
+++ src/const/settings/llm.ts
@@ -23,4 +23,4 @@ export const DEFAULT_RERANK_MODEL = 'rerank-english-v3.0';
 export const DEFAULT_RERANK_PROVIDER = 'cohere';
 export const DEFAULT_RERANK_QUERY_MODE = 'full_text';
 
-export const DEFAULT_PROVIDER = ModelProvider.OpenAI;
+export const DEFAULT_PROVIDER = ModelProvider.Qwen;
