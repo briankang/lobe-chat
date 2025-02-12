6262e4c28146899dcdce94f4546be47eb7edc30a
briankang <285524570@qq.com>
2025-02-12 20:28:41 +0800

    Update llm.ts

--- src/const/settings/llm.ts
+++ src/const/settings/llm.ts
@@ -17,10 +17,10 @@ export const DEFAULT_LLM_CONFIG = genUserLLMConfig({
 export const DEFAULT_MODEL = 'deepseek-r1';
 
 export const DEFAULT_EMBEDDING_MODEL = 'text-embedding-3-small';
-export const DEFAULT_EMBEDDING_PROVIDER = ModelProvider.OpenAI;
+export const DEFAULT_EMBEDDING_PROVIDER = ModelProvider.DeepSeek;
 
 export const DEFAULT_RERANK_MODEL = 'rerank-english-v3.0';
 export const DEFAULT_RERANK_PROVIDER = 'cohere';
 export const DEFAULT_RERANK_QUERY_MODE = 'full_text';
 
-export const DEFAULT_PROVIDER = ModelProvider.Qwen;
+export const DEFAULT_PROVIDER = ModelProvider.DeepSeek;
